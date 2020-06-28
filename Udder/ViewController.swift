da3a20f3fbc09b2c09c5337636381ecd94386f89
jkbteach2018@gmail.com <jkbteach2018@gmail.com>
2020-06-28 15:00:59 -0500

    Add labels and images

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Judith Bishop on 6/28/20.
+//  Copyright © 2020 Judith Bishop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
